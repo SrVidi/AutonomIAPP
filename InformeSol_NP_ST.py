2cce166cda72a2200301214ddfd12753fd6619ab
SrVidi <marcvidal11@hotmail.com>
2024-08-26 20:49:33 +0200

    Update InformeSol_NP_ST.py

--- InformeSol_NP_ST.py
+++ InformeSol_NP_ST.py
@@ -37,8 +37,8 @@ def main():
             if google_api_key:
                 with st.spinner("Processing document..."):
                     # Initialize language models
-                    gemini_pro = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=google_api_key)
-                    gemini_flash = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=google_api_key)
+                    gemini_pro = ChatGoogleGenerativeAI(model="gemini-1.5-pro", google_api_key=google_api_key, temperature="0.0")
+                    gemini_flash = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key=google_api_key, temperature="0.0")
 
                     # Create chains
                     chain_1 = RunnableSequence(prompt_template_WRITER, gemini_pro)
